461b24291be43e6bac907a875c52d20d1d2d3e2c
ziyamohammad <mohammad2311061@akgec.ac.in>
2025-04-05 16:36:09 +0530

    fixes in signup responsive layout

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <Login/>
-      {/* <SignUp/> */}
+      {/* <Login/> */}
+      <SignUp/>
     </div>
   );
 }
