523b3daeed157bd324d46ec1db2c975dd89b3196
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-04-05 14:21:23 +0530

    login page with fully responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
-
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+      <Login/>
+      {/* <SignUp/> */}
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import '../css/login.css';
+function Login() {
+  return (
+    <div className='main-container'>
+      <div className="left-container">
+        <div className="logo">
+          <span className="logoimg">
+            <img src="./OBJECTS.png" alt="/" height="100%" width="100%" />
+          </span>
+          <div className="logo-name">PrepWise</div>
+        </div>
+        <div className="heading">AI-powered mock interviews
+          to get you hired!</div>
+        <div className="img">
+          <img src="./signupimg.png" alt="" />
+        </div>
+        <ul className='ulbullet'>
+          <li>Practice with real AI interviewers</li>
+          <li>improve your interview skills</li>
+        </ul>
+      </div>
+      <div className="right-container">
+        <div className="signin-container">
+          <div className="signin">Sign in</div>
+          <div className="signin-heading">
+            If you don’t have an account register
+            <br />
+            You can  <span className="span-heading">Register here !</span>
+          </div>
+          <form action="">
+            <label for="email">Email</label>
+            <div className='input-container'>
+              <span className="emailimg"><img src="./message 1.png" alt="" /></span>
+              <input type="text" placeholder='Enter your email address' className="email" />
+            </div>
+            <label for="password">Password</label>
+            <div className='input-container'>
+              <span className="passwordimg"><img src="./padlock 1.png" alt="" /></span>
+              <input type="text" placeholder='Enter your Password' className="password" />
+            </div>
+            <div className="conditions">
+              <div className="remember">
+                <input type='checkbox' className='check' />
+                <span>Remember me</span>
+              </div>
+              <div className="forget">
+                Forgot Password ?
+              </div>
+            </div>
+            <button className="submit">
+              Login
+            </button>
+          </form>
+          <div className="messa">or continue with</div>
+          <div className="social-icons">
+            <a href="#" className="icon-link">
+              <img src="./Facebook.png" alt="Facebook" />
+            </a>
+            <a href="#" className="icon-link">
+              <img src="./google.png" alt="Google" />
+            </a>
+            <a href="#" className="icon-link">
+              <img src="./apple.png" alt="Apple" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Login
