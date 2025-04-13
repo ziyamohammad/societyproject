8d9236980a6baaf449e31a067cca94444cf05de9
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-04-13 12:42:16 +0530

    changes to modul.csse to avoid conflicts

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,42 +1,42 @@
 import React from 'react'
-import '../css/interface.css'
+import styles from '../css/interface.module.css'
 function Interface() {
   return (
-    <div className='main-containers'>
-        <div className="top-container">
-        <div className="logo">
-          <span className="logoimg">
+    <div className={styles["main-containers"]}>
+        <div className={styles["top-container"]}>
+        <div className={styles.logo}>
+          <span className={styles.logoimg}>
             <img src="./OBJECTS.png" alt="/" height="100%" width="100%" />
           </span>
-          <div className="logo-name">PrepWise</div>
+          <div className={styles.logoname}>PrepWise</div>
         </div>
-<div className="top-logo">
+<div className={styles["top-logo"]}>
     <img src="./apimg.png" alt="" />
 </div>
         </div>
-        <div className="second-container">
-            <div className="second-left">
+        <div className={styles["second-container"]}>
+            <div className={styles["second-left"]}>
                 <span style={{ marginRight: '0.5em' }}><img src="./image 18.png" alt="" /></span>
                  Frontend Developer Interview <span style={{ marginLeft: '0.5em' }}><img src="./tech.png" alt="" /><img src="./Frame.png" alt="" /></span>
             </div>
-            <div className="second-right">Technical Interview</div>
+            <div className={styles["second-right"]}>Technical Interview</div>
         </div>
-        <div className="boxes">
-            <div className="box">
+        <div className={styles.boxes}>
+            <div className={styles.box}>
                 <img src="./avatar-removebg-preview (1).png" alt="" />
                 <span>AI Interviewer</span>
             </div>
-            <div className="box">
+            <div className={styles.box}>
             <img src="./image (1).png" alt="" />
             <span>Adrian (You)</span>
             </div>
         </div>
-        <div className="question">
+        <div className={styles.question}>
         What job experience level are you targeting?
         </div>
-        <div className="buttons">
-          <button className="repeat"><span><img src="./repeate-music.png" alt="" /></span> Repeat</button>
-          <button className="leave" ><span><img src="./call-slash.png" alt="" /></span>Leave interview</button>
+        <div className={styles.buttons}>
+          <button className={styles.repeat}><span><img src="./repeate-music.png" alt="" /></span> Repeat</button>
+          <button className={styles.leave} ><span><img src="./call-slash.png" alt="" /></span>Leave interview</button>
         </div>
     </div>
   )
